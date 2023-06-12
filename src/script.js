b1f108cc97c7e5555cb635366e679ce19f18306f
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2023-06-12 13:08:08 +0300

    styling the piano and finshing html and css

---
+++ src/script.js
@@ -0,0 +1,14 @@
+const NOTE_DETAILS = [
+{note : "C" , key : "Z" , frequency : 261.626 },
+{note : "Db" , key : "S" , frequency : 277.786 },
+{note : "D" , key : "X" , frequency : 293.841 },
+{note : "Eb" , key : "D" , frequency : 311.456 },
+{note : "E" , key : "C" , frequency : 329.6453 },
+{note : "F" , key : "V" , frequency : 449.789 },
+{note : "Gb" , key : "G" , frequency : 465.467 },
+{note : "G" , key : "B" , frequency : 490.245 },
+{note : "Ab" , key : "H" , frequency : 510.785 },
+{note : "A" , key : "N" , frequency : 530.786 },
+{note : "Bb" , key : "J" , frequency : 560.648 },
+{note : "B" , key : "M" , frequency : 590.645 },
+]
